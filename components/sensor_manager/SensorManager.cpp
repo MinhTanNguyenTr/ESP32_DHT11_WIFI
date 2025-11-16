01a57664769f83c5144db28e01e052fd670e6af0
MinhTan <minhtannguyentr@gmail.com>
2025-11-16 22:50:52 +0700

    Create logic code for sensor manager module

--- components/sensor_manager/SensorManager.cpp
+++ components/sensor_manager/SensorManager.cpp
@@ -1,10 +1,16 @@
-/*
- * SensorManager.cpp
- *
- *  Created on: Nov 16, 2025
- *      Author: hi
- */
+#include "SensorManager.h"
+#include "esp_log.h"
 
+static const char* TAG = "SensorManager";
 
-
-
+void SensorManager::readAll() {
+    for (auto& s : sensors) {
+        if (s->read()) {
+            ESP_LOGI(TAG, "Temp = %.1f°C, Hum = %.1f%%",
+                     s->getTemperature(),
+                     s->getHumidity());
+        } else {
+            ESP_LOGW(TAG, "Failed to read sensor");
+        }
+    }
+}
--- components/sensor_manager/SensorManager.h
+++ components/sensor_manager/SensorManager.h
@@ -1,15 +1,19 @@
-/*
- * SensorManager.h
- *
- *  Created on: Nov 16, 2025
- *      Author: hi
- */
+#ifndef COMPONENTS_SENSOR_MANAGER_H_
+#define COMPONENTS_SENSOR_MANAGER_H_
 
-#ifndef COMPONENTS_SENSOR_MANAGER_SENSORMANAGER_H_
-#define COMPONENTS_SENSOR_MANAGER_SENSORMANAGER_H_
+#include <vector>
+#include "../dht11/ISensor_Interface.h"
 
+class SensorManager {
+public:
+    void addSensor(ISensor_Interface* s) {
+        sensors.push_back(s);
+    }
 
+    void readAll();
 
+private:
+    std::vector<ISensor_Interface*> sensors;
+};
 
-
-#endif /* COMPONENTS_SENSOR_MANAGER_SENSORMANAGER_H_ */
+#endif
